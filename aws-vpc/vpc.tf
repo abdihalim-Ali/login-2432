e3af5918795a33f9d0e36d94325891bdc28545b8
ravi2krishna <ravi2krishna@gmail.com>
2025-01-02 12:19:14 +0530

    create backend subnet

--- aws-vpc/vpc.tf
+++ aws-vpc/vpc.tf
@@ -17,3 +17,14 @@ resource "aws_subnet" "fe-sn" {
     Name = "login-frontend-subnet"
   }
 }
+
+# Backend Subnet
+resource "aws_subnet" "be-sn" {
+  vpc_id     = aws_vpc.vpc.id
+  cidr_block = "10.0.2.0/24"
+  map_public_ip_on_launch = "true"
+
+  tags = {
+    Name = "login-backend-subnet"
+  }
+}
