83ad697daa1b5e67bce64f4e05839a5556bff4fe
ravi2krishna <ravi2krishna@gmail.com>
2025-01-02 12:44:12 +0530

    create database security group

--- aws-vpc/vpc.tf
+++ aws-vpc/vpc.tf
@@ -200,3 +200,36 @@ resource "aws_vpc_security_group_egress_rule" "be-sg-ob" {
   cidr_ipv4         = "0.0.0.0/0"
   ip_protocol       = "-1" # semantically equivalent to all ports
 }
+
+# Database Security Group
+resource "aws_security_group" "db-sg" {
+  name        = "database-firewall"
+  description = "Allow database traffic"
+  vpc_id      = aws_vpc.vpc.id
+
+  tags = {
+    Name = "database-firewall"
+  }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "db-sg-ssh" {
+  security_group_id = aws_security_group.db-sg.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 22
+  ip_protocol       = "tcp"
+  to_port           = 22
+}
+
+resource "aws_vpc_security_group_ingress_rule" "db-sg-postgres" {
+  security_group_id = aws_security_group.db-sg.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 5432
+  ip_protocol       = "tcp"
+  to_port           = 5432
+}
+
+resource "aws_vpc_security_group_egress_rule" "db-sg-ob" {
+  security_group_id = aws_security_group.db-sg.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1" # semantically equivalent to all ports
+}
