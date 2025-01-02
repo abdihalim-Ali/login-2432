94942f8545a2e5a4e1da36ff5032b14f99b2743a
ravi2krishna <ravi2krishna@gmail.com>
2025-01-02 12:16:06 +0530

    create frontend subnet

--- aws-vpc/vpc.tf
+++ aws-vpc/vpc.tf
@@ -6,3 +6,14 @@ resource "aws_vpc" "vpc" {
     Name = "login-vpc"
   }
 }
+
+# Frontend Subnet
+resource "aws_subnet" "fe-sn" {
+  vpc_id     = aws_vpc.vpc.id
+  cidr_block = "10.0.1.0/24"
+  map_public_ip_on_launch = "true"
+
+  tags = {
+    Name = "login-frontend-subnet"
+  }
+}
