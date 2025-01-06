b9f00da8edb1391eba6b65ea12573c2d97af0dd9
ravi2krishna <ravi2krishna@gmail.com>
2025-01-06 12:33:26 +0530

    create subnets

--- az-vnet/vnet.tf
+++ az-vnet/vnet.tf
@@ -12,3 +12,26 @@ resource "azurerm_virtual_network" "lms" {
   address_space       = ["10.0.0.0/16"]
 }
 
+# Frontend Subnet
+resource "azurerm_subnet" "lms-fe-sn" {
+  name                 = "fronten-subnet"
+  resource_group_name  = azurerm_resource_group.example.name
+  virtual_network_name = azurerm_virtual_network.example.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+# Frontend Subnet
+resource "azurerm_subnet" "lms-be-sn" {
+  name                 = "backend-subnet"
+  resource_group_name  = azurerm_resource_group.example.name
+  virtual_network_name = azurerm_virtual_network.example.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+
+# Frontend Subnet
+resource "azurerm_subnet" "lms-db-sn" {
+  name                 = "database-subnet"
+  resource_group_name  = azurerm_resource_group.example.name
+  virtual_network_name = azurerm_virtual_network.example.name
+  address_prefixes     = ["10.0.3.0/24"]
+}
